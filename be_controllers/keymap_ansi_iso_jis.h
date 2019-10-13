cb481e27f885717fe851e5bfc4b5460b984eb628
Marco Vedovati <mvedovati@suse.com>
2019-10-13 20:01:03 +0200

    last mappign before mac

--- be_controllers/keymap_ansi_iso_jis.h
+++ be_controllers/keymap_ansi_iso_jis.h
@@ -8,7 +8,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_COLS][MATRIX_ROWS] = {
          ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12,   PSCR,SLCK,PAUS,                         \
          GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
          TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,  PPLS, \
-         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,                      P4,  P5,  P6,        \
+         ESC,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,                      P4,  P5,  P6,        \
          LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,        TRNS,         P1,  P2,  P3,  PENT, \
          LCTL,FN0,LALT,               SPC,                RALT,  FN0, MENU,RCTL,   TRNS,TRNS,TRNS,   P0,  PDOT),          \
     /* Layer 1: Function layer */
@@ -18,9 +18,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_COLS][MATRIX_ROWS] = {
          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LEFT,DOWN,  UP,RGHT,TRNS,TRNS,     TRNS,                      TRNS,TRNS,TRNS,      \
          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,         UP,          TRNS,TRNS,TRNS,TRNS, \
-         TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,   LEFT,DOWN,RGHT,    TRNS,TRNS)
+         TRNS,TRNS,LGUI,               TRNS,               TRNS,TRNS,TRNS,TRNS,   LEFT,DOWN,RGHT,    TRNS,TRNS),          \
 };
 
 static const action_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1)
+    [0] = ACTION_LAYER_MOMENTARY(1),
 };
